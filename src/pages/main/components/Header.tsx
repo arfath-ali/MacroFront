f21f5da0dc345e587f4168ddc4788abf83550224
arfath-ali <1290sfaa28@gmail.com>
2025-08-14 13:30:06 +0530

    feat: add tagline in header

--- src/pages/main/components/Header.tsx
+++ src/pages/main/components/Header.tsx
@@ -1,10 +1,18 @@
 const Header = () => {
   return (
-    <div className="box-border flex justify-between p-4">
-      <h1 className="text-bold">MacroFront</h1>
-      <button className="text-semibold cursor-pointer rounded-[5px] border px-2 pb-1">
-        Sign In
-      </button>
+    <div className="p-4">
+      <div className="flex justify-between">
+        <h1 className="text-bold">MacroFront</h1>
+        <button className="text-semibold cursor-pointer rounded-[5px] border px-2 pb-1">
+          Sign In
+        </button>
+      </div>
+      <div className="tablet:mt-12 mt-6 text-center">
+        <h1 className="text-bold"> Live Markets, Economic Insights and News</h1>
+        <p className="text-medium tablet:mt-4 mt-2 text-gray-900">
+          Track Trends, Stay Informed, And Visualize Data In Real Time
+        </p>
+      </div>
     </div>
   );
 };
