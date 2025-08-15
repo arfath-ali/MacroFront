7b5f22c3f1d4449e9e20d53331b3ff837dc20ef3
arfath-ali <1290sfaa28@gmail.com>
2025-08-15 10:51:03 +0530

    style: add hover effect to sign-in button

--- src/pages/main/components/Header.tsx
+++ src/pages/main/components/Header.tsx
@@ -3,12 +3,15 @@ const Header = () => {
     <div className="p-4">
       <div className="flex justify-between">
         <h1 className="text-bold">MacroFront</h1>
-        <button className="text-semibold cursor-pointer rounded-[5px] border px-2 pb-1">
+        <button className="text-semibold cursor-pointer rounded-[5px] border px-2 pb-1 transition-colors duration-200 hover:border-gray-400 hover:bg-gray-100">
           Sign In
         </button>
       </div>
       <div className="tablet:mt-16 mt-8 text-center">
-        <h1 className="text-bold text-2xl tablet:text-5xl"> Live Markets, Economic Insights and News</h1>
+        <h1 className="text-bold tablet:text-5xl text-2xl">
+          {' '}
+          Live Markets, Economic Insights and News
+        </h1>
         <p className="text-medium tablet:mt-4 mt-2 text-gray-500">
           Track Trends, Stay Informed, And Visualize Data In Real Time
         </p>
