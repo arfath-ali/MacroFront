d3c010fc5765d1379e5a28115a558e4712167c2c
arfath-ali <1290sfaa28@gmail.com>
2025-08-18 12:04:42 +0530

    chore: wrap App with ThemeProvider and CurrentPathProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { CurrentPathProvider } from 'context/CurrentPathContext.tsx';
+import { ThemeProvider } from 'context/ThemeContext.tsx';
 import App from './App.tsx';
 import './assets/styles/App.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <CurrentPathProvider>
+          <App />
+        </CurrentPathProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 );
