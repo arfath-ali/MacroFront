1e43456799363ebc237b14301e3699936c13e3fd
arfath-ali <1290sfaa28@gmail.com>
2025-08-18 12:02:05 +0530

    feat: add theme context to manage light and dark modes

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect, createContext, useContext } from 'react';
+import type { ReactNode } from 'react';
+
+interface ThemeContextType {
+  theme: string;
+  setTheme: (p: string) => void;
+  clearTheme: () => void;
+}
+const ThemeContext = createContext<ThemeContextType>({
+  theme: '',
+  setTheme: () => {},
+  clearTheme: () => {},
+});
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+const getInitialTheme = () => {
+  if (document.documentElement.classList.contains('dark')) return 'dark';
+
+  const appTheme = localStorage.getItem('Theme') || 'light';
+
+  return appTheme;
+};
+
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  const [theme, setTheme] = useState(getInitialTheme);
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
+
+  const clearTheme = () => {
+    setTheme('light');
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme, clearTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export default function useThemeContext() {
+  return useContext(ThemeContext);
+}
