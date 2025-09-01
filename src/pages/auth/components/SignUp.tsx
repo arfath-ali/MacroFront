947741741ebf33f06039498e319539a416fba2c9
arfath-ali <1290sfaa28@gmail.com>
2025-09-01 18:00:51 +0530

    refactor: improve signup form UX with contexts, field focus, aria support, and dynamic validation

--- src/pages/auth/components/SignUp.tsx
+++ src/pages/auth/components/SignUp.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ClipLoader } from 'react-spinners';
 import useUsernameContext from '@/context/UsernameContext';
+import useEmailContext from '@/context/EmailContext';
 import usePasswordContext from '@/context/PasswordContext';
 import axiosInstance from '@/services/api';
 import { log } from '@/utils/logger';
@@ -10,39 +11,45 @@ import GreenCheckIcon from '@/assets/images/icons/green-check-icon.png';
 const SignUp = () => {
   const navigate = useNavigate();
 
+  const fullNameRef = useRef<HTMLInputElement>(null);
   const [fullName, setFullName] = useState('');
   const [isFullNameFieldFocused, setIsFullNameFieldFocused] = useState<
     boolean | null
   >(null);
 
   const {
+    usernameRef,
     username,
     setUsername,
     isUsernameFieldFocused,
     setIsUsernameFieldFocused,
+    isUsernameValid,
     usernameError,
     isSearchingUsername,
     isUsernameAvailable,
     resetUsernameState,
   } = useUsernameContext();
 
-  const [email, setEmail] = useState('');
-  const [isEmailValid, setIsEmailValid] = useState<boolean | null>(null);
-  const [isEmailFieldFocused, setIsEmailFieldFocused] = useState<
-    boolean | null
-  >(null);
-  const [emailError, setEmailError] = useState('');
-
-  const [signUpLoading, setSignUpLoading] = useState(false);
-  const [signUpError, setSignUpError] = useState('');
+  const {
+    emailRef,
+    email,
+    setEmail,
+    isEmailFieldFocused,
+    setIsEmailFieldFocused,
+    isEmailValid,
+    emailError,
+    resetEmailState,
+  } = useEmailContext();
 
   const {
+    passwordRef,
     password,
     setPassword,
     isPasswordValid,
     isPasswordFieldFocused,
     setIsPasswordFieldFocused,
     passwordError,
+    confirmPasswordRef,
     confirmPassword,
     setConfirmPassword,
     isConfirmPasswordMatched,
@@ -55,6 +62,16 @@ const SignUp = () => {
   const [isAnyFieldFocused, setIsAnyFieldFocused] = useState<boolean | null>(
     null,
   );
+
+  const [isSignUpButtonClicked, setIsSignUpButtonClicked] = useState(false);
+  const [signUpLoading, setSignUpLoading] = useState(false);
+  const [signUpError, setSignUpError] = useState('');
+
+  useEffect(() => {
+    resetUsernameState();
+    resetEmailState();
+    resetPasswordState();
+  }, []);
 
   useEffect(() => {
     if (
@@ -86,35 +103,44 @@ const SignUp = () => {
     }
   }, [isUsernameAvailable]);
 
-  const validateEmail = (emailValue: string | null) => {
-    if (!emailValue) return;
-
-    setEmail(emailValue);
-
-    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const emailValidity: boolean = regex.test(emailValue);
-
-    if (emailValidity) {
-      setIsEmailValid(true);
-      setEmailError('');
-      return;
-    } else {
-      setIsEmailValid(false);
-      setEmailError('Please enter a valid email address.');
-      return;
-    }
-  };
-
   const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsSignUpButtonClicked(true);
 
     if (!fullName || !username || !email || !password || !confirmPassword) {
       setSignUpError('All fields are required.');
       return;
     }
 
-    if (isUsernameAvailable === false) {
-      setSignUpError('Username is already taken.');
+    if (!fullName) {
+      fullNameRef.current?.focus();
+      return;
+    }
+
+    if (
+      !username ||
+      isUsernameValid === false ||
+      isUsernameAvailable === false
+    ) {
+      usernameRef.current?.focus();
+
+      if (isUsernameAvailable === false)
+        setSignUpError('Username is already taken.');
+      return;
+    }
+
+    if (!email || isEmailValid === false) {
+      emailRef.current?.focus();
+      return;
+    }
+
+    if (!password || isPasswordValid === false) {
+      passwordRef.current?.focus();
+      return;
+    }
+
+    if (!confirmPassword || isConfirmPasswordMatched === false) {
+      confirmPasswordRef.current?.focus();
       return;
     }
 
@@ -138,10 +164,12 @@ const SignUp = () => {
         log(response?.data?.message);
 
         resetUsernameState();
+        resetEmailState();
         resetPasswordState();
 
         setTimeout(() => {
           setSignUpLoading(false);
+          setIsSignUpButtonClicked(false);
           navigate('/home', { replace: true });
         }, 1500);
       } catch (err: any) {
@@ -156,7 +184,11 @@ const SignUp = () => {
       onSubmit={(e) => {
         handleSignUp(e);
       }}>
-      {signUpError && <p>{signUpError}</p>}
+      {signUpError && (
+        <p id="signUpError" role="alert">
+          {signUpError}
+        </p>
+      )}
 
       <div>
         <label htmlFor="full-name">Full Name</label>
@@ -164,8 +196,13 @@ const SignUp = () => {
           type="text"
           id="full-name"
           name="full-name"
+          ref={fullNameRef}
           onFocus={() => setIsFullNameFieldFocused(true)}
-          onChange={(e) => setFullName(e.target.value)}
+          onChange={(e) => {
+            setFullName(e.target.value);
+            setSignUpError('');
+            setIsSignUpButtonClicked(false);
+          }}
           onKeyDown={(e) => {
             if (e.key === ' ') {
               if (fullName === '' || fullName.endsWith(' ')) e.preventDefault();
@@ -176,7 +213,7 @@ const SignUp = () => {
             setFullName(fullName.trim());
           }}
           value={fullName}
-          className="border-default input-default text-medium"
+          className="input-default text-medium border hover:border-gray-400 dark:border-gray-400 dark:hover:border-gray-700"
         />
       </div>
 
@@ -188,26 +225,57 @@ const SignUp = () => {
               type="text"
               id="username"
               name="username"
+              ref={usernameRef}
+              aria-describedby="usernameError"
               onFocus={() => setIsUsernameFieldFocused(true)}
-              onChange={(e) => setUsername(e.target.value)}
+              onChange={(e) => {
+                setUsername(e.target.value);
+                setSignUpError('');
+                setIsSignUpButtonClicked(false);
+              }}
               onKeyDown={(e) => {
                 if (e.key === ' ') e.preventDefault();
               }}
-              onBlur={() => setIsUsernameFieldFocused(false)}
+              onBlur={() => {
+                setIsUsernameFieldFocused(false);
+              }}
               value={username}
-              className="border-default username-input-default text-medium"
+              className={`username-input-default text-medium border ${
+                isUsernameValid === false && isUsernameFieldFocused === false
+                  ? 'border-red-600'
+                  : 'hover:border-gray-400 dark:border-gray-400 dark:hover:border-gray-700'
+              } `}
             />
             <div className="absolute top-3 right-2 flex items-center">
               {isSearchingUsername ? (
-                <ClipLoader size={15} color="#4F46E5" />
+                <>
+                  <ClipLoader size={15} color="#4F46E5" />
+                  <span className="sr-only" aria-live="polite">
+                    Checking username availability
+                  </span>
+                </>
               ) : isUsernameAvailable ? (
-                <img src={GreenCheckIcon} className="h-[15px] w-[15px]" />
+                <>
+                  <img
+                    src={GreenCheckIcon}
+                    alt="Username available"
+                    className="h-[15px] w-[15px]"
+                  />
+                  <span className="sr-only" aria-live="polite">
+                    Username available
+                  </span>
+                </>
               ) : null}
             </div>
           </div>
-          {!isAnyFieldFocused && username && !isUsernameFieldFocused && (
-            <p>{usernameError}</p>
-          )}
+          <p id="usernameError" aria-live="polite" aria-atomic="true">
+            {(isUsernameValid === false &&
+              !isAnyFieldFocused &&
+              isUsernameFieldFocused === false) ||
+            (isSignUpButtonClicked && isUsernameFieldFocused)
+              ? usernameError
+              : ''}
+          </p>
         </div>
       </div>
 
@@ -218,18 +286,28 @@ const SignUp = () => {
             type="email"
             id="email"
             name="email"
+            ref={emailRef}
+            aria-describedby="emailError"
             onFocus={() => setIsEmailFieldFocused(true)}
-            onChange={(e) => validateEmail(e.target.value)}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              setSignUpError('');
+              setIsSignUpButtonClicked(false);
+            }}
             onKeyDown={(e) => {
               if (e.key === ' ') e.preventDefault();
             }}
-            onBlur={() => setIsEmailFieldFocused(false)}
+            onBlur={() => {
+              setIsEmailFieldFocused(false);
+            }}
             value={email}
-            className="border-default input-default text-medium"
+            className="input-default text-medium border hover:border-gray-400 dark:border-gray-400 dark:hover:border-gray-700"
           />
-          {!isAnyFieldFocused && email && !isEmailFieldFocused && (
-            <p>{emailError}</p>
-          )}
+          <p id="emailError" aria-live="polite">
+            {!isAnyFieldFocused && email && !isEmailFieldFocused
+              ? emailError
+              : ''}
+          </p>
         </div>
       </div>
 
@@ -240,18 +318,28 @@ const SignUp = () => {
             type="password"
             id="password"
             name="password"
+            ref={passwordRef}
+            aria-describedby="passwordError"
             onFocus={() => setIsPasswordFieldFocused(true)}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              setSignUpError('');
+              setIsSignUpButtonClicked(false);
+            }}
             onKeyDown={(e) => {
               if (e.key === ' ') e.preventDefault();
             }}
-            onBlur={() => setIsPasswordFieldFocused(false)}
+            onBlur={() => {
+              setIsPasswordFieldFocused(false);
+            }}
             value={password}
-            className="border-default input-default text-medium"
+            className="input-default text-medium border hover:border-gray-400 dark:border-gray-400 dark:hover:border-gray-700"
           />
-          {!isAnyFieldFocused && password && !isPasswordFieldFocused && (
-            <p>{passwordError}</p>
-          )}
+          <p id="passwordError" aria-live="polite">
+            {!isAnyFieldFocused && password && !isPasswordFieldFocused
+              ? passwordError
+              : ''}
+          </p>
         </div>
       </div>
 
@@ -262,22 +350,35 @@ const SignUp = () => {
             type="password"
             id="confirm-password"
             name="confirm-password"
+            ref={confirmPasswordRef}
+            aria-describedby="confirmPasswordError"
             onFocus={() => setIsConfirmPasswordFieldFocused(true)}
-            onChange={(e) => setConfirmPassword(e.target.value)}
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+              setSignUpError('');
+              setIsSignUpButtonClicked(false);
+            }}
             onKeyDown={(e) => {
               if (e.key === ' ') e.preventDefault();
             }}
-            onBlur={() => setIsConfirmPasswordFieldFocused(false)}
+            onBlur={() => {
+              setIsConfirmPasswordFieldFocused(false);
+            }}
             value={confirmPassword}
-            className="border-default input-default text-medium"
+            className="input-default text-medium border hover:border-gray-400 dark:border-gray-400 dark:hover:border-gray-700"
           />
-          {!isAnyFieldFocused &&
+
+          <p id="confirmPasswordError" aria-live="polite">
+            {!isAnyFieldFocused &&
             confirmPassword &&
-            !isConfirmPasswordFieldFocused && <p>{confirmPasswordError}</p>}
+            !isConfirmPasswordFieldFocused
+              ? confirmPasswordError
+              : ''}
+          </p>
         </div>
       </div>
 
-      <button className="btn-auth-main-signin text-semibold">
+      <button type="submit" className="btn-auth-main-signin text-semibold">
         {signUpLoading ? <ClipLoader size={30} /> : 'Sign Up'}
       </button>
     </form>
